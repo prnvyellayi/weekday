fee672b5442f8bf57b74c289df8a5063a3500e99
Pranav <prnvshanmukh00@gmail.com>
2024-05-08 22:29:41 +0530

    chore: more card

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -5,25 +5,38 @@ import styles from "../css/App.module.css";
 import { getJobs } from "../utils/functions";
 
 const Home = () => {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState([null]);
 
   useEffect(() => {
+    let page = 0;
     const apiCall = async () => {
-      const data = await getJobs(0);
-      setData(data);
+      const data = await getJobs(page);
+      setData(data.jdList);
+      page++;
     };
     apiCall();
-  }, []);
 
+    document.addEventListener("scroll", async () => {
+      const element = document.getElementById("main");
+      const bottom =
+        element.getBoundingClientRect().bottom <= window.innerHeight;
+      if (bottom) {
+        const newdata = await getJobs(page);
+        const newDatalist = newdata.jdList
+        setData((data) => [...data, ...newDatalist]);
+        page++;
+      }
+    });
+  }, []);
   return (
-    <div className={styles.App}>
+    <div id="main" className={styles.App}>
       <Filters />
       <div className={styles.cardsbody}>
         {data &&
-          data.jdList.map((each, index) => <JobCard key={index} item={each} />)}
+          data.map((each) => <JobCard key={each?.jdUid} item={each} />)}
       </div>
     </div>
   );
-}
+};
 
 export default Home;
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "../css/card.module.css";
 
-const CompanyDetails = ({item}) => {
+const CompanyDetails = ({ item }) => {
   return (
     <div className={styles.companyMain}>
       <img src={item?.logoUrl} className={styles.logo} />
@@ -11,14 +11,23 @@ const CompanyDetails = ({item}) => {
         <span className={styles.location}>{item?.location}</span>
       </div>
     </div>
-  )
-}
+  );
+};
 
 const JobCard = ({ item }) => {
-  return <div className={styles.main}>
-    <CompanyDetails item={item} />
-    <span className={styles.salary}>Estimated Salary: {item?.minJdSalary} - {item?.maxJdSalary} LPA</span>
-  </div>;
+  return (
+    <div className={styles.main}>
+      <CompanyDetails item={item} />
+      <span className={styles.salary}>
+        Estimated Salary: {item?.minJdSalary ? `${item?.minJdSalary} -` : ''} {item?.maxJdSalary} LPA
+      </span>
+      <div className={styles.about}>
+        <span className={styles.aboutcompany}>About Company:</span>
+        <span className={styles.aboutBody}>{item?.jobDetailsFromCompany}</span>
+        <span className={styles.show}>Show more</span>
+      </div>
+    </div>
+  );
 };
 
 export default JobCard;
